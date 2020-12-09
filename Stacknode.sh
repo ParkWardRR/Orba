271d513a7cd0253be53bcfa67c67f179a12d6be5
Twesh <66848727+td147x@users.noreply.github.com>
2020-12-09 14:55:04 -0800

    Create Stacknode.sh

---
+++ Stacknode.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Running a testnet node
+
+sudo apt-get install build-essential cmake libssl-dev pkg-config git
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+
+git clone https://github.com/blockstack/stacks-blockchain.git
+cd stacks-blockchain
+
+cargo build --workspace --release --bin stacks-node
+target/release/stacks-node xenon
