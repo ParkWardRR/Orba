c61cc303f7066680dd3bb0377cbf22881a39a080
Twesh <66848727+td147x@users.noreply.github.com>
2020-12-10 15:03:01 -0800

    Update Stacknode.sh

--- Stacknode.sh
+++ Stacknode.sh
@@ -4,6 +4,9 @@
 
 sudo apt-get install build-essential cmake libssl-dev pkg-config git htop wget -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source $HOME/.cargo/env
+
+
 
 source $HOME/.cargo/env
 mkdir /tmp/stacknode/
@@ -14,13 +17,19 @@ tar -xvf /tmp/stacknode/bitcoin-0.20.1-x86_64-linux-gnu.tar.gz
 sudo cp /tmp/stacknode/bitcoin-0.20.1/bin/* /usr/local/bin/
 sudo chmod +x /usr/local/bin/*
 
-sudo mkdir /stacks
-cd /stacks
-sudo git clone https://github.com/blockstack/stacks-blockchain.git
-cd stacks-blockchain
+mkdir ~/stacks
+cd ~/stacks
+git clone https://github.com/blockstack/stacks-blockchain.git
+cd ~/stacks/stacks-blockchain/
 
-sudo cargo build --workspace --release --bin stacks-node
-sudo target/release/stacks-node xenon
+cargo build --workspace --release --bin stacks-node
+target/release/stacks-node xenon
 
-sudo mkdir /config/
-sudo touch /config/bitcoin.conf
+
+
+mkdir ~/config/
+touch ~/config/bitcoin.conf
+
+vi ~/config/bitcoin.conf
+
+bitcoind -conf=~/config/bitcoin.conf
