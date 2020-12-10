19f6c06a2f5dffeb2d6db81066b57597ff74841b
Twesh <66848727+td147x@users.noreply.github.com>
2020-12-09 17:13:43 -0800

    Update Stacknode.sh

--- Stacknode.sh
+++ Stacknode.sh
@@ -4,7 +4,12 @@
 
 sudo apt-get install build-essential cmake libssl-dev pkg-config git -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
 source $HOME/.cargo/env
+
+wget https://bitcoin.org/bin/bitcoin-core-0.20.1/bitcoin-0.20.1-x86_64-linux-gnu.tar.gz
+tar -xvf bitcoin-0.20.1-x86_64-linux-gnu.tar.gz
+sudo cp ~/bitcoin-0.20.1/bin/* /usr/local/bin/
 
 git clone https://github.com/blockstack/stacks-blockchain.git
 cd stacks-blockchain
