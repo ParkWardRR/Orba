9aa7346bcca5382952df0fb781ad933c170d29b5
Twesh <66848727+td147x@users.noreply.github.com>
2020-12-12 12:40:39 -0800

    Update Stacks.CLI.sh

--- Stacks.CLI.sh
+++ Stacks.CLI.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
+
+# node 14 
 sudo curl -sL https://deb.nodesource.com/setup_14.x | sudo bash -
 
 # https://github.com/nvm-sh/nvm
 sudo curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
 
 # install stuff
-sudo apt -y install gcc g++ make htop nodejs neofetch
-
-
-node  -v
+sudo apt -y install gcc g++ make htop nodejs neofetch wget 
 
 # https://docs.npmjs.com/resolving-eacces-permissions-errors-when-installing-packages-globally
 
@@ -16,8 +15,6 @@ mkdir ~/.npm-global
 npm config set prefix '~/.npm-global'
 export PATH=~/.npm-global/bin:$PATH
 source ~/.profile
-npm install -g jshint
-
 
 # Install stacks cli
 
